060eaf73dc9e3be505b3fb18cdd45117947a5eaf
Bonyonte <godfrey.magogo@student.moringaschool.comgit config --global user.name Bonyontegit branch -m maingit branch -M maingit remote add origin git@github.com:Bonyonte/ip_1.gitgit push -u origin maingit config --global user.email godfrey.magogo@student.moringaschool.com>
2022-06-13 13:14:28 +0300

    Done.

--- index.js
+++ index.js
@@ -59,8 +59,9 @@ const [ , , , , , indg ] = colors;
 
 const { muppetName, color, song, job, partner } = muppet;
 
-const { nestedJob, nestedPartner} = nestedMuppet;
 
 // 8. Use destructuring to assign songs 2 and 4, and Kermit's job and partner
 
 const { song2, song4 } = nestedMuppet.album.theMuppetMovie;
+
+const { nestedJob, nestedPartner} = nestedMuppet;
